946bda966c799c2337ae54685e7e6e1f6ac351cb
serstyle <desobry.f@gmail.com>
2024-09-21 13:34:03 +0200

    add read from search param

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,8 @@
 export function GET(request: Request) {
-    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+    const url = new URL(request.url);
+    const searchParam = new URLSearchParams(url.search);
+
+    const name = searchParam.get('name') || 'world';
+
+    return new Response(`Hello ${name} from ${process.env.VERCEL_REGION}`);
 }
